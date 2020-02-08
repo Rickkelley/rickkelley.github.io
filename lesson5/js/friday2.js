3df440280ef7b27b55197ffb6192d75aa07f89d1
rickkelley <rickkelley@byui.edu>
2020-02-08 13:29:17 -0700

    saturday afternoon

--- lesson5/js/friday2.js
+++
@@ -1,5 +0,0 @@
-const currentDay = getCurrentDayOfWeek();
-
-if( currentDay === "Saturday"){
-    document.querySelector('pancake').classList.add("show")
-}
--- lesson5/js/toggle_expand.js
+++
@@ -1,4 +0,0 @@
-
-function toggleExpand(){
-  document.querySelector('#nav-menu-toggle-label').classList.toggle("expand")
-}
