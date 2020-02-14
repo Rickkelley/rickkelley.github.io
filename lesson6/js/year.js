c4ba7c6dac52dac65500b24505f3feebf05c8762
rickkelley <rickkelley@byui.edu>
2020-02-13 17:32:12 -0700

    yearerrorfixed

--- lesson6/js/year.js
+++ lesson6/js/year.js
@@ -1,5 +1,5 @@
 
 
-let date = new Date();
+let date1 = new Date();
 
-document.getElementById("current-year").textContent = document.getElementById("current-year").textContent + " " + date.getFullYear();
+document.getElementById("current-year").textContent = document.getElementById("current-year").textContent + " " + date1.getFullYear();
