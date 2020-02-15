e847ad58bc051ccf88114fe1e60d3a29fc9c6de6
rickkelley <rickkelley@byui.edu>
2020-02-14 21:47:57 -0700

    fixed friday

--- lesson5/js/friday.js
+++ lesson5/js/friday.js
@@ -1,7 +1,6 @@
 
-function checkDate() {
+
     var date = new Date();
     console.log(date.getDay());
     if(date.getDay() === 5) {document.getElementById("friday").innerHTML = "Saturday is Preston Pancakes in the Park! <br> Please join us starting at 9:00 a.m. Saturday at the city park.";
     } 
-}
