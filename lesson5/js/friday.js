89c5043ca568a4a93a368d4ac047e8e6698e9db4
rickkelley <rickkelley@byui.edu>
2020-02-17 20:20:15 -0700

    monday-week7

--- lesson5/js/friday.js
+++ lesson5/js/friday.js
@@ -1,5 +1,4 @@
 
-
     var date = new Date();
     console.log(date.getDay());
     if(date.getDay() === 5) {document.getElementById("friday").innerHTML = "Saturday is Preston Pancakes in the Park! <br> Please join us starting at 9:00 a.m. Saturday at the city park.";
