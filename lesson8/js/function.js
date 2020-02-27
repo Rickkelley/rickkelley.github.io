061c090b71a5ee28663886f380f3d8d6aefb09bf
rickkelley <rickkelley@byui.edu>
2020-02-27 16:29:58 -0700

    Update function.js

--- lesson8/js/function.js
+++ lesson8/js/function.js
@@ -1,4 +1,3 @@
-function toggleMenu() {
-	
-	document.getElementById("primaryNav").classList.toggle("hide");
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
 }
