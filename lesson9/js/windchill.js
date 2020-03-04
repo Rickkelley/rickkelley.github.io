c97cc45efccf548d62f41377d0d72433b0b66a89
rickkelley <rickkelley@byui.edu>
2020-03-03 18:01:18 -0700

    index

---
+++ lesson9/js/windchill.js
@@ -0,0 +1,20 @@
+const temperature = parseFloat(document.getElementById("temp").textContent);
+
+const windspeed = parseFloat(document.getElementById("speed").textContent);
+
+let windchill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temperature * Math.pow(windspeed, 0.16));
+
+windchill = Math.round(windchill);
+
+if (temperature <51 && windspeed >3) {
+    document.getElementById("chill").textContent = "Wind Chill Temperature:  " + windchill + " ℉";
+    
+}
+
+else 
+{
+    document.getElementById("chill").textContent = "Wind Chill Temperature: N/A ";
+
+        
+
+}
---
+++ lesson9/js/year.js
@@ -0,0 +1,5 @@
+
+
+let date1 = new Date();
+
+document.getElementById("current-year").textContent = document.getElementById("current-year").textContent + " " + date1.getFullYear();
