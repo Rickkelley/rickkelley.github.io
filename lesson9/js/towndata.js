a7483cd7d6388685ee3ddac3b882e061b348a626
rickkelley <rickkelley@byui.edu>
2020-03-04 20:20:05 -0700

    commentoutlog

--- lesson9/js/towndata.js
+++ lesson9/js/towndata.js
@@ -5,7 +5,7 @@ fetch(requestURL)
     return response.json();
   })
   .then(function(jsonObject) {
-    console.table(jsonObject);	// temporary checking for valid response and data parsing
+    // console.table(jsonObject);	// temporary checking for valid response and data parsing
 	const towns = jsonObject['towns'];
 	
 let towndata = [towns[1], towns[4], towns[5]];
