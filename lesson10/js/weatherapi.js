9f1db01b52447df5d0003bd580bff5d93cb25166
rickkelley <rickkelley@byui.edu>
2020-03-10 19:14:57 -0600

    test-api

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,3 +1,9 @@
-const requestURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID={de4d915acebca03c6051dc7bcf9d513d}'
+const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=de4d915acebca03c6051dc7bcf9d513d";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+  });
 
-fetch(requestURL)
+
+
