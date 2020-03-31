a5c2c2b7aef813f8b9c0ebb019b12bbd54aee327
rickkelley <rickkelley@byui.edu>
2020-03-31 16:36:18 -0600

    josn

--- finalpage/js/guides.js
+++ finalpage/js/guides.js
@@ -2,12 +2,13 @@ const requestURL = 'https://rickkelley.github.io/finalpage/json/guides.json';
 
 
 
+
 fetch(requestURL)
   .then(function(response) {
     return response.json();
   })
   .then(function(jsonObject) {
-    //console.table(jsonObject);	// temporary checking for valid response and data parsing
+    console.table(jsonObject);	// temporary checking for valid response and data parsing
 	const guides = jsonObject['guides'];
 
 for (let i = 0; i < guides.length; i++ ) {
@@ -25,7 +26,7 @@ cert.textContent = "IRF Certification Level: " + guides[i].cert;
 email.textContent = "E-mail address: " + guides[i].email;
 bio.textContent = "Biography: " + guides[i].biography;
 img.setAttribute('src', guides[i].imageurl);
-img.setAttribute('alt', guides[i].name + ' ' + prophets[i].lastname);
+img.setAttribute('alt', guides[i].name + ' ' + guides[i].lastname);
 
 
 
